c80167fb56e040fd3fdd9a95da6f26c2e390d31f
tariq <tariq.yahya300@gmail.com>
2022-03-11 11:12:40 +0200

    omgfdadasfsds

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-/*
- *
- *
- *
- * comment
- *
- *
+/**
+ * main - Short description, single line
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
  */
-
-
 int main(void)
 {
 char c;
 	for (c = 'a'; c <= 'z'; ++c)
 
 	printf("%c", c);
+printf("\n");
 return (0);
 }
