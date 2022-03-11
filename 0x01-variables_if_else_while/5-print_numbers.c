176b65f02dbe022d749dffabaab657995efd1edb
tariq <tariq.yahya300@gmail.com>
2022-03-11 11:27:23 +0200

    blank line

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-/*
- *
- *
- * comment
- *
- *
+/**
+ * main - Short description, single line
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
  */
-
 int main(void)
 {
 	char a;
-	for (a = '0'; a <= '9'; a++)
 
+	for (a = '0'; a <= '9'; a++)
 	{
 	putchar(a);
 	}
