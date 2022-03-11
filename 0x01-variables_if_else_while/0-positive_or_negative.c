9413b242812956cf56bb9d37e11b057ad4ec2e6c
tariq <tariq.yahya300@gmail.com>
2022-03-11 11:08:37 +0200

    omg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-/*
- *
- *
- *
- *
- *
- * function main is good
- *
- *
- *
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
  */
 int main(void)
 {
