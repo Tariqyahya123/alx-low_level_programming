fb9f0e852598b5e2307c58c146024abae879670d
tariq <tariq.yahya300@gmail.com>
2022-03-11 11:45:35 +0200

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+
+int main(void)
+{	
+	int a;
+
+	for (a = 0; a < 16 ; a++)
+	{
+	printf("%x", a);
+	}
+return (0);
+}
