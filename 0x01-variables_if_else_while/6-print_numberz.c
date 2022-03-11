736cf1e0afa37347b02338f82e6744dac86a1500
tariq <tariq.yahya300@gmail.com>
2022-03-11 11:30:57 +0200

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	putchar(a + '0');
+	}
+
+return (0);
+}
