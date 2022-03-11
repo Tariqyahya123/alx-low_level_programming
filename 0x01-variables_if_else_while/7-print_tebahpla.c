d6708aa56ca4af08a98cff034783c152e4081a16
tariq <tariq.yahya300@gmail.com>
2022-03-11 11:38:58 +0200

    yeahd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+char c;
+
+	for (c = 'z'; c >= 'a' ; c--)
+	{
+	putchar(c);
+	}
+putchar('\n');
+return(0);
+
+
+}
