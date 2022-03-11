203fae3cc2203ad17c589a0dda51836ed75c81b7
tariq <tariq.yahya300@gmail.com>
2022-03-11 10:04:40 +0200

    three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ *
+ *
+ *
+ * comment
+ *
+ *
+ */
+
+
+int main(void)
+{
+char c;
+	for (c = 'a'; c <= 'z'; ++c)
+
+	printf("%c", c);
+	for (c = 'A'; c <= 'Z'; ++c)
+
+	printf("%c", c);
+printf("\n");
+return (0);
+}
