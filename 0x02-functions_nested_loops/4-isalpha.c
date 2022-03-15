37dbed935d1089fa9a484c69687f4b3aeab330e7
tariq <tariq.yahya300@gmail.com>
2022-03-16 00:45:54 +0200

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void){
+int _isalpha(int c)
+{
+ 
+
+if (  c <= 90  && c >= 65  )
+{
+return (1);
+}
+else 
+	if ( c <= 122 && c >= 97)
+{
+return (1);
+}
+return (0);
+}
+}
