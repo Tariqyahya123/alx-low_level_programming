7a1410b6f3ccee914528b336bccd03fd161b9e24
tariq <tariq.yahya300@gmail.com>
2022-03-16 01:35:52 +0200

    dsad

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - check for lower case letter
+ * Return: 0 or 1 or -1
+ *@n : character to check
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+{
+_putchar(n);
+}
+else
+{
+n = n * (-1);
+_putchar(n);
+}
+{
