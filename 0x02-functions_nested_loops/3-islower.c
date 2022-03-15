07c5a8a6f8a85fcd0e41ca8535af24bda400694a
tariq <tariq.yahya300@gmail.com>
2022-03-15 19:35:10 +0200

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,23 +7,6 @@
  */
 
 
-int _islower (int c);
-
-int main(void)
-{
-	
-int r;
-
-    r = _islower('H');
-    putchar(r + '0');
-    r = _islower('o');
-    putchar(r + '0');
-    r = _islower(108);
-    putchar(r + '0');
-    putchar('\n');
-
-    return (0);
-}
 
 int _islower(int c)
 {
