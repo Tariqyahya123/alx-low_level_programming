c39ce793d359e6d8d3dfafd0a79e3b92be2eee04
tariq <tariq.yahya300@gmail.com>
2022-03-16 01:43:41 +0200

    dAS

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - check for lower case letter
+ * Return: 0 or 1 or -1
+ *@n : character to check
+ */
+
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n);
+}
