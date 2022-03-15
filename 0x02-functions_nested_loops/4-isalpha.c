d0ec2e7bb2b9817ccc5c4d7f1ea3e2ab306df84c
tariq <tariq.yahya300@gmail.com>
2022-03-16 00:49:36 +0200

    ds

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,21 +2,21 @@
 
 /**
  * main - Entry point
- *
+ *_isalpha - check if alpha
  * Return: Always 0 (Success)
  */
 
 
 int _isalpha(int c)
 {
- 
 
-if (  c <= 90  && c >= 65  )
+
+if ( c <= 90  && c >= 65)
 {
 return (1);
 }
-else 
-	if ( c <= 122 && c >= 97)
+else
+if (c <= 122 && c >= 97)
 {
 return (1);
 }
