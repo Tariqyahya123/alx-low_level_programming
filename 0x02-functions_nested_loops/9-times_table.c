388305e82a506a344220d8fbde21f8695a348b32
tariq <tariq.yahya300@gmail.com>
2022-03-16 02:57:15 +0200

    sad

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - check for lower case letter
+ * Return: 0 or 1 or -1
+ *@n : character to check
+ */
+
+void main(void)
+{
+times_table();
+}
+
+void times_table(void)
+{
+int i;
+int j;
+int temp;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+temp = j * i;
+printf("%d", temp);
+putchar(',');
+putchar(' ');
+}
+printf("\n");
+	
+}
+}
