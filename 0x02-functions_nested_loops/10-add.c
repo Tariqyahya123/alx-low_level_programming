55544d88f580673dbe2c923e9005c9586fe55bb3
tariq <tariq.yahya300@gmail.com>
2022-03-16 03:24:09 +0200

    das

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - check for lower case letter
+ * Return: 0 or 1 or -1
+ *@a : character to check
+ *@b : character to check
+ */
+
+int add(int a, int b)
+{
+int temp;
+temp = a + b;
+return (temp);
+}
