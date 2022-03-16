737d028f2e336b11aef96ed61e5359e5b744f7ed
tariq <tariq.yahya300@gmail.com>
2022-03-16 02:32:13 +0200

    dsad

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+/**
+ * print_sign - check for lower case letter
+ * Return: 0 or 1 or -1
+ *@n : character to check
+ */
+
+int main(void){
+
+jack_bauer();
+}
+
+
+void jack_bauer(void)
+{
+int i;
+int j;
+int c;
+int d;
+
+
+for (i = 0; i < 3 ; i++)
+{
+	
+for (j = 0; j < 10; j++)
+{
+for (c = 0; c < 6; c++)
+{
+for (d = 0; d < 10; d++)
+{
+
+if (i == 2 && j == 3 && c == 5 && d == 9){
+	printf("23:59\n");
+	return ;
+}
+printf("%d%d:%d%d\n", i,j,c,d);
+}
+}
+}
+}
+}
