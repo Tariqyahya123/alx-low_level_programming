ac46de9fa02eeeddcd053d61a6d8cb4f3b8ef04c
tariq <tariq.yahya300@gmail.com>
2022-03-16 03:41:18 +0200

    das

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,8 +8,31 @@
 
 void print_to_98(int n) {
 int a;
+if (n <= 98)
+{
 for (a = n; a < 99; a++)
 {
+if (a = 98)
+{
+printf("%d", a);
+}
+else {
 printf("%d, ", a);
+}
+}
+}
+else 
+{
+for (a = n; a > 98; a--)
+{
+if (a = 98)
+{
+printf("%d", a);
+}
+else {
+printf("%d, ", a);
+
+}
+}
 }
 }
