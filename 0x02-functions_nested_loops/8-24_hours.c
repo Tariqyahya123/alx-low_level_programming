880a880fcccaf10592c5d0cb2b4589181af4f7b9
tariq <tariq.yahya300@gmail.com>
2022-03-16 02:34:42 +0200

    dsad

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <time.h>
 /**
- * print_sign - check for lower case letter
+ * jack_bauer - check for lower case letter
  * Return: 0 or 1 or -1
  *@n : character to check
  */
@@ -20,7 +20,7 @@ int d;
 
 for (i = 0; i < 3 ; i++)
 {
-	
+
 for (j = 0; j < 10; j++)
 {
 for (c = 0; c < 6; c++)
@@ -28,9 +28,10 @@ for (c = 0; c < 6; c++)
 for (d = 0; d < 10; d++)
 {
 
-if (i == 2 && j == 3 && c == 5 && d == 9){
+if (i == 2 && j == 3 && c == 5 && d == 9)
+{
 	printf("23:59\n");
-	return ;
+	return;
 }
 printf("%d%d:%d%d\n", i,j,c,d);
 }
