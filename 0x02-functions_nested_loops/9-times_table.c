3e6b3409840f2fc004f1ae9efefacd0199388680
tariq <tariq.yahya300@gmail.com>
2022-03-16 03:05:02 +0200

    sad

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,13 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
+	
 temp = j * i;
+if (temp >= 10){
+printf("%d", temp);
+putchar(',');
+putchar(' ');	
+}
 printf("%d", temp);
 putchar(',');
 putchar(' ');
