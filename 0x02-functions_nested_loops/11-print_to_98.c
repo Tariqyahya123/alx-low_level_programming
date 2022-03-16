8f313491eefda58abd5dd1b369570302a0bc133f
tariq <tariq.yahya300@gmail.com>
2022-03-16 03:27:03 +0200

    das

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - check for lower case letter
+ *@n : character to check
+ */
+
+void print_to_98(int n)
+{
+for (n < 98; n++)
+{
+printf("%d, ", n);
+}
+}
