69b292b48a184bc0c93b81667ebb2575b62ac22a
tariq <tariq.yahya300@gmail.com>
2022-03-17 22:16:13 +0200

    sda

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints numbers
+ * Return : 0
+ */
+
+
+void more_numbers(void)
+{
+char i;
+int j = 1;
+
+while (j <= 10){
+for (i = 0; i < 15; i++)
+{
+if (i > 9){	
+_putchar((i/10)+'0');
+_putchar((i%10)+'0');
+}
+else
+{
+_putchar('0' + i);
+}
+}
+_putchar('\n');
+j++;
+}
+
+}
+
