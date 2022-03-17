71ca9f383e2a317f18add8cc5a9d59a8561cb964
tariq <tariq.yahya300@gmail.com>
2022-03-17 12:19:10 +0200

    sda

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ *
+ * @c :  character to check the case
+ * Return: 0 or 1
+ */
+
+
+int mul(int a, int b)
+{
+return (a*b);
+}
