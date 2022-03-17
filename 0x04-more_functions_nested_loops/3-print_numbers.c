430c7f4d5829cb1789d947749e7f90ef516be479
tariq <tariq.yahya300@gmail.com>
2022-03-17 13:48:54 +0200

    sda

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ */
+
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+}
