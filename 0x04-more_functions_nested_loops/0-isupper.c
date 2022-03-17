9254ba442d86ea9fee3834f3a4b8f86df295d243
tariq <tariq.yahya300@gmail.com>
2022-03-17 11:28:42 +0200

    sdfsd

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Entry point
+ *
+ * Return: Always 0 (Success)
+ * @c :  yes man
+ */
+
+
+int _isupper(int c)
+{
+if (c >= 65 && c < 97)
+{
+return (1);
+}
+return (0);
+
+}
