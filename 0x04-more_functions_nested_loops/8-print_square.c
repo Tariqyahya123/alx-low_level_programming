3aa42d20599157cfa492d7f6dbf6b6d731af4304
tariq <tariq.yahya300@gmail.com>
2022-03-17 23:52:50 +0200

    sda

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,9 @@ void print_square(int size)
 {
 int i;
 int j;
+
+if(size > 0)
+{
 for (i =0; i < size ; i++)
 {
 for (j=0; j < size; j++)
@@ -20,3 +23,9 @@ _putchar('\n');
 }
 
 }
+else
+{
+_putchar('\n');
+}
+}
+
