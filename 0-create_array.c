27cb9faed3a70706d25617b49aa8074f0d772cf2
tariqyahya123 <tariq.yahya300@gmail.com>
2022-04-06 01:33:34 +0200

    afads

---
+++ 0-create_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+char array[99];
+
+int i;
+
+for (i = 0; i < (int) size; i++)
+{
+array[i] = c;
+}
+return 'c';
+}
